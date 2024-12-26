f7d2820cd74625042ee5e1ec95b0b758e8a4a92d
Hesham Adel <hesham.adel.dwedar@gmail.com>
2024-12-26 16:25:37 +0200

    adds main layout and font

---
+++ src/app/components/timer.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+interface TimerProps {
+  initialValue: number;
+  onExpire?: () => void;
+  running?: boolean;
+}
+
+const Timer: React.FC<TimerProps> = ({ initialValue, onExpire, running }) => {
+  const [time, setTime] = useState(initialValue);
+
+  useEffect(() => {
+    if (time > 0 && running) {
+      const timerId = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(timerId);
+    } else {
+      if (onExpire) {
+        onExpire();
+      }
+    }
+  }, [time, onExpire, running]);
+
+  return (
+    <div className="flex justify-center align-center text-9xl" >
+      <p>{formatTime(time)}</p>
+    </div>
+  );
+};
+
+const formatTime = (time: number) => {
+  return `${String(Math.floor(time / 60)).padStart(2, '0')}:${String(time % 60).padStart(2, '0')}`;
+};
+
+export default Timer;
